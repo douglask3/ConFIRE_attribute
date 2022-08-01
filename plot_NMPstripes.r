1ce16c17f0b3a22802210b732629c7458126584f
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2022-08-01 14:55:22 +0100

    doing a binned temp vs NBP plot

--- plot_NMPstripes.r
+++ plot_NMPstripes.r
@@ -34,7 +34,7 @@ cols2 = c('#c7eae5','#80cdc1','#35978f','#01665e','#003c30')
 
 years = as.numeric(dat[2,-1])
 years[1] = years[2] - 1
-forRegions <- function(region, Rid, yearsTest = TRUE) {
+forRegions <- function(region, Rid, yearsTest = TRUE, stripes = FALSE) {
     if (region == "Global") idR = which(dat[,1] == "GlobTemp")
     else idR = which(dat[,2] == region)
     
@@ -42,10 +42,6 @@ forRegions <- function(region, Rid, yearsTest = TRUE) {
     else xs = temps#lapply(temps, function(i)  i[tyrID,-1])
     xrange = range(unlist(xs))
     
-    plot(xrange, c(0, 14), axes = FALSE, 
-         ylim = c(13, 0), xlab = '', ylab = '', type = 'n')
-    
-    #axis(1)
     modelNBP <- function(model) {
         idM = which(as.character(dat[,2]) == model)
         id = idR[idR > idM][1]
@@ -54,6 +50,57 @@ forRegions <- function(region, Rid, yearsTest = TRUE) {
     }
 
     nbps = lapply(models, modelNBP)
+
+    if (!stripes) {
+        
+        test_temps = seq(floor(min(unlist(temps))*10)/10,
+                         ceiling(max(unlist(temps))*10)/10, by = 0.1)
+
+        binExp <- function(id) {
+            binT <- function(t1, t2) {
+                binMod <- function(nbp, temp) {                 
+                    test = temp > t1 & temp <= t2
+                    c(mean(nbp[id, test]), sum(test))
+                }
+                out = mapply(binMod, nbps, temps)
+                
+                c(mean(t1, t2), sum(out[2,]),
+                  range(out[1,], na.rm = TRUE), mean(out[1,], na.rm = TRUE))
+            }
+            out = mapply(binT, head(test_temps, -1), test_temps[-1])
+
+        }
+        bins = lapply( 1:2, binExp)
+        
+        addPoly <- function(dat, col) {
+            polygon(c(dat[1,], rev(dat[1,])), c(dat[4,], rev(dat[3,])), 
+                   col = make.transparent(col, 0.95), border =NA)# make.transparent(col, 0.95))
+            
+            lineLWD <- function(x, y, lwd, ...) 
+                lapply(1:length(x),
+                       function(i) lines(x[(i-1):i], y[(i-1):i], lwd = lwd[(i-1):i], ...))
+            lineLWD(dat[1,], dat[5,], col = make.transparent(col, 0.6), 
+                    lwd = 3*sqrt(dat[2,]/max(dat[2,])))
+        }
+        
+        plot(range(test_temps), range(unlist(lapply(bins, function(i) i[-(1:2),]))), 
+              axes = FALSE, xlab = '', ylab = '', type = 'n')    
+        axis(2)
+        axis(1)
+        grid()
+        for (i in 1:3) {
+        mapply(addPoly, bins, c('blue', 'red'))
+        mapply(addPoly, rev(bins), rev(c('blue', 'red')))
+        }
+        lines(c(-9E9, 9E9), c(0, 0), lty = 2, lwd = 2)
+        mtext(side = 3, adj = 0.1, region, line = -1.7)
+        return()
+    } 
+    plot(xrange, c(0, 14), axes = FALSE, 
+        ylim = c(13, 0), xlab = '', ylab = '', type = 'n')
+
+    #axis(1)
+    
     dnps = lapply(nbps, function(i) i[2,] - i[1,])
     
     testKicker <- function(x, temp, syrs = 2000, offset = 10) {
@@ -135,6 +182,23 @@ forRegions <- function(region, Rid, yearsTest = TRUE) {
     axis(1)
    # if (Rid > (length(regions) -4)) axis(1)
 }
+png("figs/NBP_vs_temp.png", res = 300, units = 'in', height = 12, width = 12)
+par(mfrow = c(4, 4), mar = c(1.5, 1.5, 0, 1.5), oma = c(2.75, 3.0, 0.25, 0.0))
+mapply(forRegions, regions, 1:length(regions), TRUE)
+mtext.units(outer = TRUE, side = 1, '~DELTA~ T (~DEG~C)', line = 1.25)
+mtext.units(outer = TRUE, side = 2, 'NBP (PgC ~yr-1~)', line = 0.75)
+plot.new()
+col = c("blue", "red")
+par(mar = c(1, 4, 7, 1))
+for (i in 1:6) {
+legend("topleft", c("Without fire", "With fire"), lwd = 1.5, bty = 'n',
+       col = make.transparent(col, 0.6))
+legend("topleft", c("Without fire", "With fire"), lwd = 10, bty = 'n',
+       col = make.transparent(col, 0.95))
+}
+dev.off()
+
+stop()
 
 png("figs/NBPstripes.png", res = 300, units = 'in', height = 14, width = 12)
 par(mfrow = c(4, 4), mar = c(1.5, 1.5, 0, 1.5), oma = c(1.5, 1, 1, 1.5))
