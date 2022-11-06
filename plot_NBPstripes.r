b305cc9794e0ab9850c4abfae4bf4e5794bec3c7
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2022-11-06 08:48:28 +0000

    allowing flexibality over base period. Looking at cumlative change to GWLs. Applying smoothung to NBP vs temp TS and cutting off TS at point where first model stops

--- plot_NBPstripes.r
+++ plot_NBPstripes.r
@@ -18,8 +18,10 @@ models = c("HadGEM2-ES" = "H", "GFDL" = "G", "IPSL" = "I", "MIROC" = "M")
 regions = c("Global", "BONA", "TENA", "CEAM", "NHSA", "SHSA", "EURO", "MIDE", 
                       "NHAF", "SHAF", "BOAS", "CEAS", "SEAS", "EQAS", "AUST")
 
+accumulative = FALSE
 
-
+start_year = 2000
+nyears = 20
 modTemp <- function(model) {
     id = which(dat[,2] == model) + 1
     if (model == 'H') id = id + 1
@@ -42,22 +44,51 @@ forRegions <- function(region, Rid, yearsTest = TRUE, stripes = FALSE) {
     else xs = temps#lapply(temps, function(i)  i[tyrID,-1])
     xrange = range(unlist(xs))
     
-    modelNBP <- function(model) {
+    modelNBP <- function(model, accTest) {
         idM = which(as.character(dat[,2]) == model)
         id = idR[idR > idM][1]
         
-        apply(dat[id+(2:1), -1], 2, as.numeric)
+        out = apply(dat[id+(2:1), -1], 2, as.numeric)
+        if (accTest) {
+            out = t(apply(out, 1, cumsum))
+            for (i in 1:2) out[i,] = out[i,] - out[i, years == 2008]
+        }
+        return(out)
     }
 
-    nbps = lapply(models, modelNBP)
+    nbps = lapply(models, modelNBP, accumulative)
+    tnbps = lapply(models, modelNBP, FALSE)
+
+    dnps = lapply(nbps, function(i) i[2,] - i[1,])
+    tdnps =  lapply(tnbps, function(i) i[2,] - i[1,])
+    testKicker <- function(x, temp, syrs = start_year, offset = 10, xs = tdnps, id) {
+        
+        syr = which(years == syrs)
+        x0 = x[syr - 1 +  1:nyears]
+        doTest <- function(i) 
+            wilcox.test(x0, xs[[1]][i + 1:nyears], paired = FALSE)[[3]]
+
+        pvs = sapply(syr:(length(x) -nyears), doTest)
+        #if (id == 3) browser()
+        sig = which(pvs < 0.1)[1] + offset
+        c(sig + syrs + nyears/2, temp[sig + syr])
+    }
+    
+    fireSig = mapply(testKicker, tdnps, temps, id = 1:3, SIMPLIFY = FALSE)
+    
+    ddnps = lapply(tdnps, function(x) sapply(20:length(x), function(i) mean(x[(i-19):i])))
+    fireSig_an = mapply(testKicker, ddnps, temps, MoreArgs = list(xs = ddnps), SIMPLIFY = FALSE)
+    
 
     if (!stripes) {
         
         test_temps = seq(floor(min(unlist(temps))*10)/10,
-                         ceiling(max(unlist(temps))*10)/10, by = 0.1)
+                         min(sapply(temps, max))-0.05, by = 0.1)
 
         binExp <- function(id) {
-            binT <- function(t1, t2) {
+            binT <- function(t1, dt = 0.1) {
+                t1 = t1 - dt/2
+                t2 = t1 + dt
                 binMod <- function(nbp, temp) {                 
                     test = temp > t1 & temp <= t2
                     c(mean(nbp[id, test]), sum(test))
@@ -67,7 +98,7 @@ forRegions <- function(region, Rid, yearsTest = TRUE, stripes = FALSE) {
                 c(mean(t1, t2), sum(out[2,]),
                   range(out[1,], na.rm = TRUE), mean(out[1,], na.rm = TRUE))
             }
-            out = mapply(binT, head(test_temps, -1), test_temps[-1])
+            out = mapply(binT, test_temps)
 
         }
         bins = lapply( 1:2, binExp)
@@ -79,44 +110,36 @@ forRegions <- function(region, Rid, yearsTest = TRUE, stripes = FALSE) {
             lineLWD <- function(x, y, lwd, ...) 
                 lapply(1:length(x),
                        function(i) lines(x[(i-1):i], y[(i-1):i], lwd = lwd[(i-1):i], ...))
+            
             lineLWD(dat[1,], dat[5,], col = make.transparent(col, 0.6), 
                     lwd = 3*sqrt(dat[2,]/max(dat[2,])))
         }
-        
-        plot(range(test_temps), range(unlist(lapply(bins, function(i) i[-(1:2),]))), 
+        yrange = unlist(lapply(bins, function(i) i[-(1:2),]))
+        yrange = yrange[!is.na(yrange) & !is.infinite(yrange)]
+        yrange = range(yrange)
+        plot(range(test_temps), yrange, 
               axes = FALSE, xlab = '', ylab = '', type = 'n')    
         axis(2)
         axis(1)
         grid()
+        xpol = range(sapply(fireSig, function(i) i[2]))
+        polygon(xpol[c(1, 2, 2, 1, 1)], yrange[c(1,1,2,2,1)], col = '#00000011', border = NA)
+        lapply(xpol, function(x) lines(c(x, x), yrange, lty = 2))
         for (i in 1:3) {
-        mapply(addPoly, bins, c('blue', 'red'))
-        mapply(addPoly, rev(bins), rev(c('blue', 'red')))
+            mapply(addPoly, bins, c('blue', 'red'))
+            mapply(addPoly, rev(bins), rev(c('blue', 'red')))
         }
         lines(c(-9E9, 9E9), c(0, 0), lty = 2, lwd = 2)
         mtext(side = 3, adj = 0.1, region, line = -1.7)
+        
         return()
-    } 
+    }
+    
     plot(xrange, c(0, 14), axes = FALSE, 
         ylim = c(13, 0), xlab = '', ylab = '', type = 'n')
 
     #axis(1)
     
-    dnps = lapply(nbps, function(i) i[2,] - i[1,])
-    
-    testKicker <- function(x, temp, syrs = 2000, offset = 10) {
-        syr = which(years == syrs)
-        x0 = x[syr+ 1:20]
-        doTest <- function(i) 
-            wilcox.test(x0, dnps[[1]][i + 1:20], paired = FALSE)[[3]]
-
-        pvs = sapply(syr:(length(x) -20), doTest)
-        sig = which(pvs < 0.1)[1] + offset
-        c(sig + syrs + 10, temp[sig + syr])
-    }
-    fireSig = mapply(testKicker, dnps, temps, SIMPLIFY = FALSE)
-    
-    ddnps = lapply(dnps, function(x) sapply(20:length(x), function(i) mean(x[(i-19):i])))
-    fireSig_an = mapply(testKicker, ddnps, temps, SIMPLIFY = FALSE)
     
     findColsLims <- function(dats) {
         limits = sort(c(0, quantile(dats, seq(0.1, 0.9, 0.1))))
@@ -137,17 +160,19 @@ forRegions <- function(region, Rid, yearsTest = TRUE, stripes = FALSE) {
     }
     c(limits, cols) := findColsLims(unlist(nbps))
     c(dlimits, dcols) := findColsLims(unlist(dnps))
-   
+    
     forModel <- function(model, mi, x, mark1, mark2) {
         idM = which(dat[,2] == model)
         id = idR[idR > idM][1]
-        nbp = dat[id+(2:1), ]
+        nbp = nbps[[which(models == model)]]#dat[id+(2:1), ]
+        #if (accumulative) nbp = cumsum(as.numeric(nbp))
         forExp <- function(i, cols, limits) {
             y = mi + (i-1) * (length(models)+0.5) 
             y = rep(y, length(x))
             if (i == 3) col = as.numeric(nbp[2,-1]) - as.numeric(nbp[1,-1])
             else col = as.numeric(nbp[i,-1])
             col = cut_results(col, limits) 
+            
             plotLines <- function(x, y, col, lwd = 3, ...) 
                 lines(c(x, x), y + c(-0.5, 0.5), col = col, lend = 1, lwd = lwd, ...)
             cols.pt = cols[col]  
@@ -158,13 +183,16 @@ forRegions <- function(region, Rid, yearsTest = TRUE, stripes = FALSE) {
                     text(x = mark[1], y = y[1], col = col,
                         adj = c(0.5, adj), round(mark[2], 2), srt = srt)
                 }
-                addMark(mark1, 2); addMark(mark2, 3, "white", adj = -0.2, srt = 90)
+                
+                addMark(mark1, 2)#; addMark(mark2, 3, "white", adj = -0.2, srt = 90)
             }
         }
+        
         forExp(1, cols, limits)        
         forExp(2, cols, limits)      
         forExp(3, dcols, dlimits)
     }
+    
     mapply(forModel, models, 1:length(models), xs, fireSig, fireSig_an)
     
     xx = xrange[2] + diff(xrange) * c(0.017,0.05)
@@ -180,9 +208,40 @@ forRegions <- function(region, Rid, yearsTest = TRUE, stripes = FALSE) {
         textSt(11.5, 'Difference')
     }
     axis(1)
-   # if (Rid > (length(regions) -4)) axis(1)
+
+    out = matrix(unlist(fireSig), nrow = 2)
+    colnames(out) = models
+    rownames(out) = paste0(region, '-', c('Year', 'Temp'))
+    outi = apply(out, 1, range)
+    if (any(is.na(outi))) outi = apply(out, 1, function(i) c(min(i, na.rm = TRUE), NA))
+    outi = as.vector(outi)
+    names(outi) = c('year-min', 'year-max', 'tas-min', 'tas-max')
+    outi[3:4] = round(outi[3:4], 2)
+    
+    if (region == "Global") {
+        sigTemp = rbind(out, 
+                        mapply(function(yr, nbp) diff(nbp[,which(years == yr)]), out[1,], nbps))
+
+        emssionsAtTemp <- function(tas) {
+            forModel <- function(temp, nbp) {
+                i = which.min(abs(temp - tas))
+                diff(nbp[,i])
+            }
+            out = mapply(forModel, temps, nbps)
+            return(round(out))
+        }
+        tass = seq(1.1, 2.0, by = 0.1)
+        ipccTemp = sapply(tass, emssionsAtTemp)
+        colnames(ipccTemp) = tass
+        write.csv(ipccTemp, file = 'outputs/allowableEmissins_change.csv')
+    }
+    return(outi)
+
+    #if (Rid > (length(regions) -4)) axis(1)
 }
-png("figs/NBP_vs_temp.png", res = 300, units = 'in', height = 12, width = 12)
+
+figname <- function(nme) paste0("figs/", nme, "-accu_", accumulative, ".png")
+png(figname("NBP_vs_temp"), res = 300, units = 'in', height = 12, width = 12)
 par(mfrow = c(4, 4), mar = c(1.5, 1.5, 0, 1.5), oma = c(2.75, 3.0, 0.25, 0.0))
 mapply(forRegions, regions, 1:length(regions), TRUE)
 mtext.units(outer = TRUE, side = 1, '~DELTA~ T (~DEG~C)', line = 1.25)
@@ -198,9 +257,11 @@ legend("topleft", c("Without fire", "With fire"), lwd = 10, bty = 'n',
 }
 dev.off()
 
-stop()
 
-png("figs/NBPstripes.png", res = 300, units = 'in', height = 14, width = 12)
+
+png(figname("NBPstripes"), res = 300, units = 'in', height = 14, width = 12)
 par(mfrow = c(4, 4), mar = c(1.5, 1.5, 0, 1.5), oma = c(1.5, 1, 1, 1.5))
-mapply(forRegions, regions, 1:length(regions), TRUE)
+outs = mapply(forRegions, regions, 1:length(regions), TRUE, TRUE)
+write.csv(t(outs), file = 'temp_yrs.csv')
+
 dev.off()
